aee284e536984c6742e1f3d9d4162feb7545d046
Felipe Menendez Roco <162356244+k00cker@users.noreply.github.com>
2025-05-26 21:51:48 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React from "react";
-import NavBar from "./components/NavBar";
-import ItemListContainer from "../src/components/ItemListContainer";
-import "primereact/resources/themes/lara-light-blue/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ItemListContainer from "./components/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import Cart from "./components/Cart";
+import ContactForm from "./components/ContactForm";
+import NotFound from "./components/NotFound";
+import { CartProvider } from "./contexts/CartContext";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <NavBar />
-      <ItemListContainer />
-    </div>
+    <CartProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/item/:itemId" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </CartProvider>
   );
-};
+}
 
 export default App;
